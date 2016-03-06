509853c39dfa972ed3044d6675064cdaf8393cb4
alexstar1995 <alex.stratulat@adycad.com>
2016-03-06 05:31:40 +0000

    song works with video

--- static/js/controller/appController.js
+++ static/js/controller/appController.js
@@ -12,9 +12,17 @@ angular.module('mainApp')
 				.then(function(response) {
 	
 					$scope.videoUrl = response[0];
-					$scope.song     = response[1];
+					$scope.song = response[1];
+					console.log($scope.song);
 					$scope.view2 = true;
+					startMusic();
 				});
+		}
+
+		$scope.startMusic = function(songName) {
+
+			var audio = new Audio('../static/music/' + songName);
+			audio.play(); 
 		}
 	}
 ]);
